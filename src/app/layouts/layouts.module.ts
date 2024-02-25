3644cbe23df8c7accaf4f62b13fd58d2e17130ba
Kristian Hadzhikolev <kris@bull44.com>
2024-02-25 03:13:24 +0200

    added default layout

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { AppRoutingModule } from '../app-routing.module';
+import { ComponentsModule } from '../components/components.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { DefaultComponent } from './default/default.component';
+
+const components = [DefaultComponent];
+
+@NgModule({
+  declarations: [...components],
+  imports: [
+    ComponentsModule,
+    AppRoutingModule,
+    BrowserModule,
+    // PagesRoutingModule
+  ],
+  exports: [...components],
+  providers: [],
+})
+export class LayoutsModule {}
