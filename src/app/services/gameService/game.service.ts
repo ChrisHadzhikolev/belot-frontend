1593f0bfbbffc30476f077280077433b9b2962eb
Kristian Hadzhikolev <kris@bull44.com>
2024-04-30 22:45:24 +0300

    added the new backend connections

--- src/app/services/gameService/game.service.ts
+++ src/app/services/gameService/game.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map } from 'rxjs';
 import { BaseApiService } from '../base/base.service';
-import { Player } from 'src/app/models/player/player.model';
+import { Game } from 'src/app/models/game/game.model';
 import { ApiResponse } from 'src/app/models/ApiResponse/api-response.model';
 
 const httpOptions = {
@@ -12,23 +12,34 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root',
 })
-export class UsersService {
-  getCurrentUser() {
-    const url = this.baseService.getBackendApiUrl() + 'users/current';
-    return this.http.get(
-        url,
-        {headers: this.baseService.getHeaders(true)}
-    );
-  }
-  constructor(private baseService: BaseApiService, private http: HttpClient) {}
+export class GameService {
+    constructor(private baseService: BaseApiService, private http: HttpClient) {}
 
-  verifyCode(secretCode: string): Observable<any> {
-    const url = this.baseService.getBackendApiUrl() + 'users/secret';
-    return this.http.post(
-        url,
-      {
-        secretCode: secretCode
-      },
+    getAllGames(): Observable<any> {
+        const url = this.baseService.getBackendApiUrl() + `data/game/all`;
+        const headers = this.baseService.getHeaders(true);
+        return this.http.get(url, {headers}).pipe(
+            //@ts-ignore
+          map((result: ApiResponse) => {
+            return result.data as Game[];
+          }),
+          catchError(() => {
+            throw new Error('Something went wrong, could not get the games.');
+          }),
+        );
+      }
+
+  saveGame(game: Game): Observable<any> {
+    const url = this.baseService.getBackendApiUrl() + 'data/game';
+    const headers = this.baseService.getHeaders(true);
+    return this.http.post(url, { game }, {headers}).pipe(
+        //@ts-ignore
+      map((result: ApiResponse) => {
+        return result.data as Game;
+      }),
+      catchError(() => {
+        throw new Error('Something went wrong, could not save the game.');
+      }),
     );
   }
 
@@ -68,19 +79,5 @@ export class UsersService {
   logout() {
     const url = this.baseService.getBackendApiUrl() + 'users/logout';
     return this.http.post(url, {}, httpOptions);
-  }
-
-  getAllPlayers(): Observable<any> {
-    const url = this.baseService.getBackendApiUrl() + `users/all`;
-    const headers = this.baseService.getHeaders(true);
-    return this.http.get(url, {headers}).pipe(
-        //@ts-ignore
-      map((result: ApiResponse) => {
-        return result.data as Player[];
-      }),
-      catchError(() => {
-        throw new Error('Something went wrong, could not get the players.');
-      }),
-    );
   }
 }
--- src/app/services/teamService/team.service.ts
+++ src/app/services/teamService/team.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, catchError, map } from 'rxjs';
 import { BaseApiService } from '../base/base.service';
-import { Player } from 'src/app/models/player/player.model';
+import { Game } from 'src/app/models/game/game.model';
+import { Team } from 'src/app/models/team/team.model';
 import { ApiResponse } from 'src/app/models/ApiResponse/api-response.model';
 
 const httpOptions = {
@@ -12,25 +13,35 @@ const httpOptions = {
 @Injectable({
   providedIn: 'root',
 })
-export class UsersService {
-  getCurrentUser() {
-    const url = this.baseService.getBackendApiUrl() + 'users/current';
-    return this.http.get(
-        url,
-        {headers: this.baseService.getHeaders(true)}
-    );
-  }
-  constructor(private baseService: BaseApiService, private http: HttpClient) {}
+export class TeamService {
+    constructor(private baseService: BaseApiService, private http: HttpClient) {}
+    getAllTeams(): Observable<any> {
+      const url = this.baseService.getBackendApiUrl() + `data/team/all`;
+      const headers = this.baseService.getHeaders(true);
+      return this.http.get(url, {headers}).pipe(
+          //@ts-ignore
+        map((result: ApiResponse) => {
+          return result.data as Team[];
+        }),
+        catchError(() => {
+          throw new Error('Something went wrong, could not get the teams.');
+        }),
+      );
+    }
 
-  verifyCode(secretCode: string): Observable<any> {
-    const url = this.baseService.getBackendApiUrl() + 'users/secret';
-    return this.http.post(
-        url,
-      {
-        secretCode: secretCode
-      },
-    );
-  }
+    createTeam(team: Team): Observable<any> {
+      const url = this.baseService.getBackendApiUrl() + `data/team`;
+      const headers = this.baseService.getHeaders(true);
+      return this.http.post(url, {team}, {headers}).pipe(
+          //@ts-ignore
+        map((result: ApiResponse) => {
+          return result.data as Team;
+        }),
+        catchError(() => {
+          throw new Error('Something went wrong, could not save the team.');
+        }),
+      );
+    }
 
   checkUsername(username: string): Observable<any> {
     const url = this.baseService.getBackendApiUrl() + 'users/username';
@@ -68,19 +79,5 @@ export class UsersService {
   logout() {
     const url = this.baseService.getBackendApiUrl() + 'users/logout';
     return this.http.post(url, {}, httpOptions);
-  }
-
-  getAllPlayers(): Observable<any> {
-    const url = this.baseService.getBackendApiUrl() + `users/all`;
-    const headers = this.baseService.getHeaders(true);
-    return this.http.get(url, {headers}).pipe(
-        //@ts-ignore
-      map((result: ApiResponse) => {
-        return result.data as Player[];
-      }),
-      catchError(() => {
-        throw new Error('Something went wrong, could not get the players.');
-      }),
-    );
   }
 }
